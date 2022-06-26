fddb440774274abed21d27e6fef19f02b58c29b1
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-06-25 21:25:30 -0300

    Initial commit

---
+++ src/main/java/com/infnet/padroesprojeto/PadroesProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.padroesprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoApplication.class, args);
+	}
+
+}
